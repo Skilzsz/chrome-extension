8fdfb5aaf8af067c732e48530bd80995e912a819
Jesse Rosengrant <jebsi@Jesses-MacBook-Air.local>
2023-11-02 16:13:49 -0400

    got popup working

--- popup.js
+++ popup.js
@@ -1,17 +1,24 @@
 // logic for radio buttons in popup
-const onButton = document.querySelector("#button-on")
-const offButton = document.querySelector("#button-off")
-const happyWill = document.querySelector("#happy")
-const angryWill = document.querySelector("#angry")
-angryWill.style = "display: none"
-if (onButton.checked) {
-    happyWill.style = "display: block"
-    angryWill.style = "display: none"
-    }   
-if (offButton.checked) {
-    happyWill.style="display: none"
-    angryWill.style = "display: block"
-} else {
-    happyWill.style = "display: block"
-    angryWill.style = "display: none"
-}
+document.addEventListener('DOMContentLoaded', () => {
+    const onButton = document.querySelector("#button-on")
+    const offButton = document.querySelector("#button-off")
+    const happyWill = document.querySelector("#happy")
+    const angryWill = document.querySelector("#angry")
+
+    angryWill.style.display = "none"
+
+    onButton.addEventListener("change", () => {
+        if (onButton.checked) {
+        happyWill.style = "display: block"
+        angryWill.style = "display: none"
+        }   
+    })
+    
+    offButton.addEventListener("change", () => {
+        if (offButton.checked) {
+            happyWill.style = "display: none"
+            angryWill.style = "display: block"
+        }
+    })
+    }
+)
