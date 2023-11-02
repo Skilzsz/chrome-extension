f55b14599b3dbfb1a088dd525c9690ee21187e05
Jesse Rosengrant <jebsi@Jesses-MacBook-Air.local>
2023-11-02 17:09:59 -0400

    got popup to save prior state, still need to get it to actually turn off functionality

--- popup.js
+++ popup.js
@@ -1,5 +1,5 @@
 // logic for radio buttons in popup
-document.addEventListener('DOMContentLoaded', () => {
+// document.addEventListener('DOMContentLoaded', () => {
     const onButton = document.querySelector("#button-on")
     const offButton = document.querySelector("#button-off")
     const happyWill = document.querySelector("#happy")
@@ -7,18 +7,36 @@ document.addEventListener('DOMContentLoaded', () => {
 
     angryWill.style.display = "none"
 
-    onButton.addEventListener("change", () => {
-        if (onButton.checked) {
-        happyWill.style = "display: block"
-        angryWill.style = "display: none"
-        }   
-    })
-    
-    offButton.addEventListener("change", () => {
-        if (offButton.checked) {
-            happyWill.style = "display: none"
-            angryWill.style = "display: block"
+    // use chrome api to load state of popup buttons from last time they were changed
+    chrome.storage.local.get(["switchState"], (result) => {
+        console.log(switchState);
+        if (result.switchState === 'on') {
+            onButton.checked = true;
+            happyWill.style.display = "block"
+            angryWill.style.display = "none"
+        } else {
+            offButton.checked = true;
+            happyWill.style.display = "none"
+            angryWill.style.display = "block"
         }
     })
-    }
-)
+
+    // when buttons change state, hide other picture and set state in chrome storage
+    onButton.addEventListener("change", () => {
+        if (onButton.checked) {
+            happyWill.style.display = "block"
+            angryWill.style.display = "none"
+            chrome.storage.local.set({switchState: "on"}).then((result) => {console.log(result.switchState)})
+        }  
+        
+    })
+
+    offButton.addEventListener("change", () => {
+        if (offButton.checked) {
+            happyWill.style.display = "none"
+            angryWill.style.display = "block"
+            chrome.storage.local.set({switchState: "off"})
+        }
+        
+    })
+// })
